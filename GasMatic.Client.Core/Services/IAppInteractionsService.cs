87871c99dc56348adee03b9044d07923de70a95b
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-18 08:16:43 +0100

    move AppInteractionsService class to separate file

--- GasMatic.Client.Core/Services/IAppInteractionsService.cs
+++ GasMatic.Client.Core/Services/IAppInteractionsService.cs
@@ -5,34 +5,3 @@ public interface IAppInteractionsService
     Task OpenBrowserAsync(string url);
     Task ComposeSupportEmailAsync(string recipient, string subject);
 }
-
-public class AppInteractionsService : IAppInteractionsService
-{
-    public async Task OpenBrowserAsync(string url)
-    {
-        try
-        {
-            var uri = new Uri(url);
-            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
-        }
-        catch (Exception ex)
-        {
-            // An unexpected error occurred. No browser may be installed on the device.
-        }
-    }
-
-    public async Task ComposeSupportEmailAsync(string recipient, string subject)
-    {
-        if (!Email.Default.IsComposeSupported)
-            return;
-
-        var message = new EmailMessage
-        {
-            Subject = subject,
-            BodyFormat = EmailBodyFormat.PlainText,
-            To = [recipient]
-        };
-
-        await Email.Default.ComposeAsync(message);
-    }
-}
