badbfd7826007a58001cb968ce4a1f38b4979b69
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-25 07:23:51 +0100

    add loading and show snackbar when deleting data

--- GasMatic.Client.Core/ViewModels/SettingsViewModel.cs
+++ GasMatic.Client.Core/ViewModels/SettingsViewModel.cs
@@ -4,30 +4,24 @@ using CommunityToolkit.Mvvm.Messaging;
 using DevExpress.Maui.Controls;
 using GasMatic.Client.Core.Features.GasVolume.Database;
 using GasMatic.Client.Core.Messages;
-using GasMatic.Client.Core.Services;
+using GasMatic.Client.Core.Services.Alerts;
+using GasMatic.Client.Core.Services.Interactions;
 
 namespace GasMatic.Client.Core.ViewModels;
 
-public partial class SettingsViewModel : ObservableObject
+public partial class SettingsViewModel(
+    IAppInteractionsService appInteractionsService,
+    IGasVolumeDatabase gasVolumeDatabase,
+    IAlertService alertService) : ObservableObject
 {
-    private readonly IAppInteractionsService _appInteractionsService;
-    private readonly IGasVolumeDatabase _gasVolumeDatabase;
-
     private const string NominalPipeSizeUrl = "https://en.wikipedia.org/wiki/Nominal_Pipe_Size";
 
     [ObservableProperty] private BottomSheetState _deleteDataSheetState = BottomSheetState.Hidden;
     [ObservableProperty] private double _deleteSliderValue;
-
-    public SettingsViewModel(
-        IAppInteractionsService appInteractionsService,
-        IGasVolumeDatabase gasVolumeDatabase)
-    {
-        _appInteractionsService = appInteractionsService;
-        _gasVolumeDatabase = gasVolumeDatabase;
-    }
+    [ObservableProperty] private bool _isLoading;
 
     [RelayCommand]
-    private async Task OpenNpsWebLink() => await _appInteractionsService.OpenBrowserAsync(NominalPipeSizeUrl);
+    private async Task OpenNpsWebLink() => await appInteractionsService.OpenBrowserAsync(NominalPipeSizeUrl);
 
     [RelayCommand]
     private void ShowDeleteDataSheet()
@@ -38,14 +32,14 @@ public partial class SettingsViewModel : ObservableObject
     [RelayCommand]
     private async Task DeleteAllData()
     {
-        // TODO: use loading?
-        // await delete
-        await _gasVolumeDatabase.DeleteAllAsync();
+        IsLoading = true;
+        await gasVolumeDatabase.DeleteAllAsync();
 
         DeleteDataSheetState = BottomSheetState.Hidden;
         DeleteSliderValue = 0;
+        IsLoading = false;
 
-        // TODO: Send Message to Gas History VM that all data locally has been deleted.
         WeakReferenceMessenger.Default.Send(new GasVolumeDataDeletedMessage());
+        await alertService.ShowSnackbarAsync("Your data has successfully been deleted.");
     }
 }
