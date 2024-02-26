771f3dd795a4f3b14a1c834d4b61414dd27f0d77
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-02-26 17:51:26 +0100

    customize entry handler to remove underline on Android

--- GasMatic.Mobile/App.xaml.cs
+++ GasMatic.Mobile/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Localization;
+#if ANDROID
+using Android.Content.Res;
+#endif
 
 namespace GasMatic.Mobile;
 
@@ -8,6 +11,14 @@ public partial class App : Application
     {
         localizationManager.RestorePreviousCulture();
         Application.Current.UserAppTheme = AppTheme.Dark;
+
+        // Remove underline from Android Entry control
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, _) =>
+        {
+#if ANDROID
+            handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#endif
+        });
 
         InitializeComponent();
 
