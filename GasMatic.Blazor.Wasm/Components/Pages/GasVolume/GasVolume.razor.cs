932abf435482242af1b5cb55e8bd29374f9692bf
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-07-19 21:45:34 +0200

    move code to code-behind class

---
+++ GasMatic.Blazor.Wasm/Components/Pages/GasVolume/GasVolume.razor.cs
@@ -0,0 +1,20 @@
+using GasMatic.Blazor.Wasm.ViewModels;
+
+namespace GasMatic.Blazor.Wasm.Components.Pages.GasVolume;
+
+public partial class GasVolume
+{
+    private enum GasVolumeTab
+    {
+        Calculator,
+        History
+    }
+
+    private GasVolumeInputViewModel _inputViewModel = new();
+    private GasVolumeTab _tab = GasVolumeTab.Calculator;
+
+    private void ChangeTabTo(GasVolumeTab tab)
+    {
+        _tab = tab;
+    }
+}
